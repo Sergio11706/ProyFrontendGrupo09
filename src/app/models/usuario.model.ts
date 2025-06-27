70d6f326e69814b031d911f1b3eb0b6f88ada191
Sergio <sergioacu17@gmail.com>
2025-06-27 01:36:18 -0300

    Correciones en models/usuario y service/usuario

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,10 +1,70 @@
 export interface Usuario {
-  _id?: string;
+  _id: string;
   username: string;
+  password: string;
   nombre: string;
   apellido: string;
+  estado: boolean;
   email: string;
   telefono: string;
   activo: boolean;
-  tipo?: 'admin' | 'cliente' | 'repartidor';
+  tipoUsuario: 'Administrador' | 'Cliente' | 'Repartidor';
+}
+
+
+export class Administrador implements Usuario {
+  tipoUsuario: 'Administrador' = 'Administrador';
+  constructor(
+    public _id: string,
+    public username: string,
+    public password: string,
+    public nombre: string,
+    public apellido: string,
+    public estado: boolean,
+    public email: string,
+    public telefono: string,
+    public activo: boolean,
+    public permisos: number,
+    public ultimaModificacion: string
+  ) {}
+}
+
+export class Cliente implements Usuario {
+  tipoUsuario: 'Cliente' = 'Cliente';
+  constructor(
+    public _id: string,
+    public username: string,
+    public password: string,
+    public nombre: string,
+    public apellido: string,
+    public estado: boolean,
+    public email: string,
+    public telefono: string,
+    public activo: boolean,
+    public barrio: string,
+    public calle: string,
+    public numeroCalle: number,
+    public descuento: number
+  ) {}
+}
+
+export class Repartidor implements Usuario {
+  tipoUsuario: 'Repartidor' = 'Repartidor';
+  constructor(
+    public _id: string,
+    public username: string,
+    public password: string,
+    public nombre: string,
+    public apellido: string,
+    public estado: boolean,
+    public email: string,
+    public telefono: string,
+    public activo: boolean,
+    public documento: string,
+    public numeroLicencia: string,
+    public domicilio: string,
+    public vehiculo: string,
+    public zonaAsignada: string,
+    public rating: number
+  ) {}
 }
